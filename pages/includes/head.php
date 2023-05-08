ed808258d64e82329bbc2389e70aae1b02f8620e
AsukaTanaka <125338448+LuckyFlame@users.noreply.github.com>
2023-05-08 22:15:18 +0700

    Update

--- pages/includes/head.php
+++ pages/includes/head.php
@@ -9,5 +9,6 @@
 <link rel="stylesheet" href="/php-travelvn/assets/scss/style.scss">
 <!-- Script Loader -->
 <script type="text/javascript" src="/php-travelvn/assets/js/loader.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 <!-- Script Ajax Bootstrap -->
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
